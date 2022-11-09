2996c810aeae7c531f126dd0fcffa10a7f94a1b8
pg-7v <dmkseev@gmail.com>
2022-11-09 11:58:37 +0300

    feat: added endpoints for users

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import PostDetail, PostList
+from .views import PostDetail, PostList, UserList, UserDetail
 
 urlpatterns = [
+    path('users/', UserList.as_view()),
+    path('users/<int:pk>/', UserDetail.as_view()),
     path('<int:pk>/', PostDetail.as_view()),
     path('', PostList.as_view()),
+
 ]
